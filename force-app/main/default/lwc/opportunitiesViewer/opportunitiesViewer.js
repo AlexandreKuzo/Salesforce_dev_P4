e3e146592c756e57619f759be8864243a848594f
alexandrekuzo <alexandre.kuzo@gmail.com>
2025-04-25 17:37:43 +0200

    Add columns and buttons + profile user method on LWC

--- force-app/main/default/lwc/opportunitiesViewer/opportunitiesViewer.js
+++ force-app/main/default/lwc/opportunitiesViewer/opportunitiesViewer.js
@@ -1,18 +1,67 @@
 import { LightningElement, track, wire, api } from 'lwc';
 import getLineItems from '@salesforce/apex/OpportunityLineItemViewer.getLineItems';
+import { getRecord } from 'lightning/uiRecordApi';
+import USER_ID from '@salesforce/user/Id';
+import PROFILE_NAME from '@salesforce/schema/User.Profile.Name';
 
 export default class OpportunitiesViewer extends LightningElement {
     @api recordId;
     @track lineItems;
+    @track isAdmin = false;
 
-    columns = [
-        { label: 'Nom du produit', fieldName: 'productName' },
-        { label: 'Quantité', fieldName: 'quantity', type: 'number' },
-        { label: 'Prix unitaire', fieldName: 'unitPrice', type: 'currency' },
-        { label: 'Prix total', fieldName: 'totalPrice', type: 'currency' },
-        { label: 'Quantité en stock', fieldName: 'productQuantityInStock', type: 'number' }
-    ];
+    @wire(getRecord, {
+        recordId: USER_ID,
+        fields: [PROFILE_NAME]
+    })
+    userProfileInfo({ error, data }) {
+        if (data) {
+            const profile = data.fields.Profile.displayValue || '';
+            this.isAdmin = ['System Administrator'].includes(profile);
+        } else if (error) {
+            console.error('Erreur lors de la récupération du profil :', error);
+        }
+    }
 
+    get columns() {
+        let columns = [
+            { label: 'Nom du produit', fieldName: 'productName' },
+            { label: 'Quantité', fieldName: 'quantity', type: 'number' },
+            { label: 'Prix unitaire', fieldName: 'unitPrice', type: 'currency' },
+            { label: 'Prix total', fieldName: 'totalPrice', type: 'currency' }, 
+            {
+                label: 'Supprimer',
+                type: 'button-icon',
+                typeAttributes: {
+                    iconName: 'utility:delete',
+                    label: 'Supprimer',
+                    name: 'delete',
+                    title: 'Cliquer pour agir',
+                    variant: 'brand',
+                    class: 'slds-m-left_xx-small'
+                }
+            }
+        ];
+    
+        if (this.isAdmin) {
+            columns.push(
+                {
+                label: 'Voir produit',
+                type: 'button',
+                typeAttributes: {
+                    label: 'View Product',
+                    iconName: 'utility:preview',
+                    name: 'delete',
+                    title: 'Cliquer pour agir',
+                    variant: 'brand'
+                }
+            }
+        
+        );
+        }
+    
+        return columns;
+    }
+   
 
     @wire(getLineItems, { opportunityId: '$recordId' })
     wiredLineItems({ error, data }) {
@@ -26,4 +75,6 @@ export default class OpportunitiesViewer extends LightningElement {
     get hasLineItems() {
         return this.lineItems && this.lineItems.length > 0;
     }
+
+   
 }
